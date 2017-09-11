9b7ac0a52f29061d8ba9dd8d0f93f3f6b992d9b8
fregu856 <fregu856@student.liu.se>
2017-09-11 22:12:01 +0200

    File structure is up, a very basic simulated test area works.

---
+++ catkin_ws/build/Balrog/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/fregu856/TSRT10_sim/catkin_ws/src/Balrog
+# Build directory: /home/fregu856/TSRT10_sim/catkin_ws/build/Balrog
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/fregu856/TSRT10_sim/catkin_ws/src
+# Build directory: /home/fregu856/TSRT10_sim/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(Balrog)
+subdirs(asl_turtlebot)
